15ff2a63ed970c28c00b81ff6f8ad6addb72a05e
josephBabs <bajosd@gmail.com>
2026-02-16 00:42:38 +0100

    update changes

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,14 @@
-import Navbar from "../components/Navbar";
-import { Outlet } from "react-router-dom";
+// // import Navbar from "../components/Navbar";
+// import { Outlet } from "react-router-dom";
 
-export default function AppLayout() {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navbar />
+// export default function AppLayout() {
+//   return (
+//     <div className="min-h-screen flex flex-col bg-gray-50">
+//       <Navbar />
 
-      <main className="flex-1 w-full">
-        <Outlet />
-      </main>
-    </div>
-  );
-}
+//       <main className="flex-1 w-full">
+//         <Outlet />
+//       </main>
+//     </div>
+//   );
+// }
