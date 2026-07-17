8b42e76770f00f669156428b3872d4a381b1703a
josephBabs <bajosd@gmail.com>
2026-07-18 01:59:02 +0200

    update

--- functions/api/founders/config.ts
+++ functions/api/founders/config.ts
@@ -2,5 +2,5 @@ import { json } from "../../_lib/http";
 import type { PortalEnv } from "../../_lib/types";
 
 export function onRequestGet({ env }: { env: PortalEnv }) {
-  return json({ productUrl: env.CHARIOW_FOUNDERS_PRODUCT_URL || "" });
+  return json({ productUrl: env.CHARIOW_FOUNDERS_PRODUCT_URL || "https://dzrkqyqp.mychariow.shop/prd_htdw78o8" });
 }
